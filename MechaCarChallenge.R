313d170a5456d884591e57e099c0b489e73eebb0
sechiller <103475613+sechiller@users.noreply.github.com>
2022-07-31 11:38:09 -0700

    Add files via upload
    + Deliverable 2+3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+##Deliverable 1
+
 #Use the library() function to load the dplyr package.
 library(dplyr)
 
@@ -7,4 +9,33 @@ mpg_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F
 #generate multiple linear regression model
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_table) 
 
+#summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_table))
+
+
+##Deliverable 2
+coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+?summarize
+#create summary table
+total_summary <- coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=sd(PSI)^2,SD=sd(PSI), .groups = 'keep') 
+total_summary
+
+#group summary table by lot
+lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=sd(PSI)^2,SD=sd(PSI), .groups = 'keep') 
+lot_summary
+
+
+#Deliverable 3
+?t.test()
+#set up t.test
+t.test(coil_table$PSI,mu=mean(coil_table$PSI))
+
+#set up t.test for subsets
+Coil_Lot1 = subset(coil_table, Manufacturing_Lot == "Lot1")
+Coil_Lot2 = subset(coil_table, Manufacturing_Lot == "Lot2")
+Coil_Lot3 = subset(coil_table, Manufacturing_Lot == "Lot3")
+
+t.test(Coil_Lot1$PSI,mu=mean(coil_table$PSI))
+t.test(Coil_Lot2$PSI,mu=mean(coil_table$PSI))
+t.test(Coil_Lot3$PSI,mu=mean(coil_table$PSI))
